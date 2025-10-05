0b4299558db5aa2e7fdc2bbbe4bb31b84764f3e2
ASUS <ASUS@DESKTOP-ND0JR3E>
2025-10-05 20:51:47 +0700

    nop lab3

--- Lab3/src/main/java/com/mycompany/lab3/bai1.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai1.java
@@ -8,9 +8,34 @@ package com.mycompany.lab3;
  *
  * @author ICT
  */
-public class bai1 {
 
+import java.util.Scanner;
+
+public class bai1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int N;
+        boolean ok = true;
+
+        System.out.print("Nhap mot so nguyen: ");
+        N = sc.nextInt();
+
+        if (N < 2) {
+            ok = false;
+        } else {
+            for (int i = 2; i < N; i++) {
+                if (N % i == 0) {
+                    ok = false;
+                    break;
+                }
+            }
+        }
+
+        if (ok)
+            System.out.println(N + " la so nguyen to.");
+        else
+            System.out.println(N + " khong phai la so nguyen to.");
+
+        sc.close();
     }
 }
--- Lab3/src/main/java/com/mycompany/lab3/bai4.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai4.java
@@ -8,9 +8,55 @@ package com.mycompany.lab3;
  *
  * @author ICT
  */
-public class bai4 {
+import java.util.Scanner;
 
+public class bai4 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so luong sinh vien: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] hoTen = new String[n];
+        double[] diem = new double[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap ho ten sinh vien thu " + (i+1) + ": ");
+            hoTen[i] = sc.nextLine();
+
+            System.out.print("Nhap diem cua sinh vien thu " + (i+1) + ": ");
+            diem[i] = sc.nextDouble();
+            sc.nextLine();
+        }
+        System.out.println("\nDanh sach sinh vien:");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Ho ten: %s, Diem: %.2f, Hoc luc: %s\n",
+                              hoTen[i], diem[i], hocLuc(diem[i]));
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (diem[j] > diem[j + 1]) {
+                    double tempDiem = diem[j];
+                    diem[j] = diem[j + 1];
+                    diem[j + 1] = tempDiem;
+                    String tempTen = hoTen[j];
+                    hoTen[j] = hoTen[j + 1];
+                    hoTen[j + 1] = tempTen;
+                }
+            }
+        }
+        System.out.println("\nDanh sach sinh vien sau khi sap xep theo diem tang dan:");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Ho ten: %s, Diem: %.2f, Hoc luc: %s\n",
+                              hoTen[i], diem[i], hocLuc(diem[i]));
+        }
+
+        sc.close();
+    }
+    public static String hocLuc(double diem) {
+        if (diem < 5) return "Yeu";
+        else if (diem < 6.5) return "Trung binh";
+        else if (diem < 7.5) return "Kha";
+        else if (diem < 9) return "Gioi";
+        else return "Xuat sac";
     }
 }
